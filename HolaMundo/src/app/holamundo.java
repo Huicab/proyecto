60b3621558d98068223f2fb6abcc777925f86001
PC <PC@Huicab>
2021-10-12 22:23:07 -0500

    Updated

---
+++ HolaMundo/src/app/holamundo.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package app;
+
+/**
+ * @author PC
+ *
+ */
+public class holamundo {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
